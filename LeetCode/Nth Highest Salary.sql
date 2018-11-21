c8a80c56c7ed337911e792f5fb659f658fed24e6
Varun Upadhyay <varun.u28@gmail.com>
2018-11-20 19:07:43 -0800

    Create Nth Highest Salary.sql

---
+++ LeetCode/Nth Highest Salary.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+SET N=N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT DISTINCT Salary 
+      FROM Employee
+      ORDER BY Salary DESC
+      LIMIT 1 
+      OFFSET N
+  );
+END
